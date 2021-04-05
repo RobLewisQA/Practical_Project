0cf858c0e27421b7b26b5e7cd5f78af41baf1199
RobLewisQA <rlewis@academytrainee.com>
2021-04-05 23:44:01 +0000

    rectified the form submission

--- back-end/application/routes.py
+++ back-end/application/routes.py
@@ -11,10 +11,10 @@ def hello():
   return render_template('home.html', data1=data1)
 
 
-@app.route('/users/add', methods=['GET','POST'])
-def add_user():
-    requests.get('http://frontend:5003/posted').text
-  return render_template('add_user.html')
+# @app.route('/users/add', methods=['GET','POST'])
+# def add_user():
+#     requests.get('http://frontend:5003/posted').text
+#     return render_template('add_user.html')
 
 @app.route('/added', methods=['GET'])
 def receive_data():
@@ -24,14 +24,15 @@ def receive_data():
 @app.route('/add',methods=['GET','POST'])
 def add_users():
     if request.method=='POST':
-      response = requests.get('http://frontend:5003/posted').text
-      new_f_name = response.split(" ")[0]
-      new_l_name = response.split(" ")[1]
-      new_eml = response.split(" ")[2]
+      content = request.json
+      #response = requests.get('http://frontend:5003/posted').text
+      new_f_name = content["new_first_name"]#response.split(" ")[0]
+      new_l_name = content['new_last_name']#response.split(" ")[1]
+      new_eml = content['new_email']#response.split(" ")[2]
       new_num = requests.get('http://random_numbers:5001/rnum').text
       new_let = requests.get('http://random_letters:5002/rletters').text
       new_number = str(new_num)+new_let
-      if int(new_num) < 600:
+      if int(new_num) < 300:
         new_user = Users(first_name=new_f_name,last_name=new_l_name,email=new_eml,rand_number=new_number,win_lose='winner')
         db.session.add(new_user)
         db.session.commit()
